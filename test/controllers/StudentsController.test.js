acf7d01861cc2f8e9188c00e0601f3a5b9582a3b
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-02 13:27:51 -0500

    Test para obtener todos los estudiantes de la db Controller

---
+++ test/controllers/StudentsController.test.js
@@ -0,0 +1,10 @@
+const Reader = require("../../lib/utils/Reader");
+const StudentsController = require("../../lib/controllers/StudentsController");
+
+describe("StudentsController", () => {
+    test("getAllStudents", () => {
+        const data = Reader.readJsonFile("data/visualpartners.json");
+        const result = StudentsController.getAllStudents();
+        expect(result).toEqual(data);
+    });
+});
