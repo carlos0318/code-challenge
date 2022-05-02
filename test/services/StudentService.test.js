3431fdbe82c32c1c32a0962998fd094afc1b7f8c
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-02 13:09:29 -0500

    Test agregado para obtener todos los estudiantes

---
+++ test/services/StudentService.test.js
@@ -0,0 +1,11 @@
+const Reader = require("../../lib/utils/Reader");
+const StudentService = require("../../lib/services/StudentService");
+
+describe("StudentService", () => {
+    test("getAllStudents", () => {
+        const data = Reader.readJsonFile("test/data/test.json");
+        const students = StudentService.getAllStudents(data);
+        expect(students.length).toBe(5);
+    });
+
+});
