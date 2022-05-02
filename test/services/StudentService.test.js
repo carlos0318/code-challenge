e8538394462c0cb3c794694a76af8275cd24b12a
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-02 13:13:23 -0500

    Test para obtener los estudiantes con mas de 500 creditos

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -13,4 +13,10 @@ describe("StudentService", () => {
         const students = StudentService.getStudentsByHaveCertification(data);
         expect(students.length).toBe(4);
     });
+
+    test("getStudentsBy500Credits", () => {
+        const data = Reader.readJsonFile("test/data/test.json");
+        const students = StudentService.getStudentsBy500Credits(data);
+        expect(students.length).toBe(3);
+    });
 });
